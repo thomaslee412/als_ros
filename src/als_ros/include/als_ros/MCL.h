c67ea4195b8900f83391f3ce23142fab4d21b88e
Thomas Lee <60892000+thomaslee412@users.noreply.github.com>
2023-10-12 16:42:18 +0800

    changed invalidScanRate to 35% as always 26% invalid data

--- src/als_ros/include/als_ros/MCL.h
+++ src/als_ros/include/als_ros/MCL.h
@@ -156,7 +156,7 @@ public:
             ROS_DEBUG_STREAM("invalidScanNum: " << (double)invalidScanNum);
             ROS_DEBUG_STREAM("scanSize: " << (int)scan_.ranges.size());
             ROS_DEBUG_STREAM("invalidScanRate: " << invalidScanRate);
-            if (invalidScanRate > 0.25) {
+            if (invalidScanRate > 0.35) {
                 scanMightInvalid_ = true;
                 ROS_DEBUG("MCL scan might invalid.");
             } else {
@@ -551,7 +551,7 @@ public:
 
         if (rejectUnknownScan_ && (measurementModelType_ == 0 || measurementModelType_ == 1))
             rejectUnknownScan();
-            
+
         double xo = baseLink2Laser_.getX();
         double yo = baseLink2Laser_.getY();
         double yawo = baseLink2Laser_.getYaw();
